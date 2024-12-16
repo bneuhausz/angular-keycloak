29ed4646a52093856487bdfe50b4205e04180ebc
nb <bneuhausz@gmail.com>
2024-12-16 22:43:35 +0100

    added user-management-component

---
+++ src/app/user-management/user-management.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  imports: [],
+  providers: [],
+  template: `
+    <main>
+      <h1>User Management</h1>
+    </main>
+  `,
+  styles: []
+})
+export default class UserManagementComponent {}
